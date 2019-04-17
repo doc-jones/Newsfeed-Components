344405b8c04b9528130c9c8524492d5f99e6d968
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-17 18:22:59 -0400

    Add Article class, expand Btn

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -21,7 +21,7 @@ const toggleMenu = () => {
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelectorAll(".menu");
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelectorAll(".menu-button");
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', () => {toggleMenu();});
